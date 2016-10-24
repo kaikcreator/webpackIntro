ffbb0c113a1605fa04969bdd07a949323ebe76b8
-KaiK- <enriqueoriol@gmail.com>
2016-10-24 20:05:54 +0200

    compiling SaSS with webpack

--- app/index.js
+++ app/index.js
@@ -1,8 +1,11 @@
 //import Logger
 var Logger = require('./logger.js');
 
+//import CSS
+require('../styles.scss');
+
 //update content of greeting element when DOM has been loaded
 document.addEventListener('DOMContentLoaded', function(){
-	document.getElementById('greeting').innerHTML = "Hello my happy friend!";
+	document.getElementById('greeting').innerHTML = "Hello my friend!";
 	Logger("Greeting has been updated");
 })
--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,10 @@ module.exports = {
 	output: {
 		filename: 'bundle.js',
 		path: __dirname
+	},
+	module: {
+		loaders: [
+			{test:/\.scss$/, loader:'style!css!sass', exclude: /node_modules/}
+		]
 	}
 };
